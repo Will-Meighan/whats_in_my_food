a726e35078e90606cc177d347d3a53815db1728c
William Meighan <williamdmeighan@gmail.com>
2020-03-30 10:40:29 -0600

    Clean up and improve test

--- spec/features/user_can_search_for_food_spec.rb
+++ spec/features/user_can_search_for_food_spec.rb
@@ -4,18 +4,23 @@ require 'rails_helper'
 feature "user can search for food" do
 
   scenario "user submits valid food type" do
-    # As a user
-    # When I visit "/"
     visit '/'
 
     fill_in :q, with: "sweet potatoes"
-    # And I select "Colorado" from the dropdown
-    click_on "Search"
-    # And I click on "submit"
-    expect(current_path).to eq(foods_path)
-    # Then my path should be "/search" with "foods=sweet potatoes" in the parameters
-    expect(page).to have_content("10 Results")
-    # And I should see a message "7 Results"
 
+    click_on "Search"
+
+    expect(current_path).to eq(foods_path)
+
+    expect(page).to have_content("10 Results")
+
+    expect(page).to have_css(".food", count: 10)
+
+    within(first(".food")) do
+      expect(page).to have_css(".description")
+      expect(page).to have_css(".brandOwner")
+      expect(page).to have_css(".ingredients")
+      expect(page).to have_css(".gtinUpc")
+    end
   end
 end
