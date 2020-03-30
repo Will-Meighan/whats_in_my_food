bcbfa70e06f8aed265b3f72d1a184a01e55fbaa2
William Meighan <williamdmeighan@gmail.com>
2020-03-30 10:08:32 -0600

    search controller working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,13 @@ class SearchController < ApplicationController
   def index
     q = params[:q]
 
-    conn = Faraday.new(url: "https://fdc.nal.usda.gov/") do |faraday|
+    conn = Faraday.new(url: "https://api.nal.usda.gov/") do |faraday|
       faraday.headers["FOOD-DATA-API-KEY"] = 'UKykCvjqPXCGPwTvorRAGS4imEV12e8z5kxqEUgy'
     end
 
     response = conn.get("/fdc/v1/search?api_key=UKykCvjqPXCGPwTvorRAGS4imEV12e8z5kxqEUgy\&generalSearchInput=#{q}")
 
     json = JSON.parse(response.body, symbolize_names: true)
-    @foods = json[:results]
-
-
+    @foods = json[:foods]
   end
 end
